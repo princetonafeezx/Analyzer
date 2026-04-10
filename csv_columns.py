a71f8214b3b3e55c5bbced96f6cde4e8af561960
PrincetonAfeez <princetonafeez@gmail.com>
2026-04-10 15:35:20 -0700

    score the date column

--- csv_columns.py
+++ csv_columns.py
@@ -10,4 +10,20 @@ from textutil import clean_text
 def _header_tokens(norm: str) -> set[str]:
     return set(re.findall(r"[a-z0-9]+", norm))
 
-
+def _score_date(norm: str, tokens: set[str]) -> float:
+    for phrase in (
+        "transaction date",
+        "posting date",
+        "posted date",
+        "post date",
+        "trans date",
+        "value date",
+        "book date",
+    ):
+        if phrase in norm:
+            return 14.0
+    if norm in {"date", "dt"}:
+        return 13.0
+    if "date" in tokens:
+        return 9.0
+    return 0.0
